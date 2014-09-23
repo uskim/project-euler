dc0172d36af10f5c7f5249f5946af3e82abb1913
git-user <git-user@localhost>
2014-09-23 04:43:28 +0200

    added C solution with XOR experiments

---
+++ 2/p2.c
@@ -0,0 +1,46 @@
+/*
+ * not exactly optimal solution, but one with simple experiments with XOR
+ * wikipedia: xor "tells whether there is an odd number of 1 bits"
+ */
+#include <stdio.h>
+
+#define MAX_FIB 4000000 
+
+// determine parity from parities of sum elements
+unsigned sum_parity(unsigned a, unsigned b);
+
+int main()
+{
+    // parity: 1 - odd, 2 - even
+    unsigned a = 1, b = 2, a_sign = 1, b_sign = 2, sum = 0;
+
+    while(a <= MAX_FIB)
+    {
+        //printf("a: %d, b: %d\n",a,b);
+        if(a_sign == 2)
+            sum += a;
+
+        // determine signs of numbers from next iteration
+        b_sign = a_sign ^ b_sign; 
+        a_sign = a_sign ^ b_sign;
+        b_sign = a_sign ^ b_sign;
+        b_sign = sum_parity(a_sign, b_sign);
+
+        // find next element in Fib sequence
+        b = a ^ b;
+        a = a ^ b;
+        b = a ^ b;
+        b += a;
+    }
+    
+    printf("sum = %d\n", sum);
+
+    return 0;
+}
+
+unsigned sum_parity(unsigned a, unsigned b)
+{
+    if(a == b)
+        return 2;
+    return 1;
+}
